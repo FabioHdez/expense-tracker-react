afad1f081c9ddf9174be23eb3b4ca0e562ecf272
Fabio Hernandez <44535804+FabioHdez@users.noreply.github.com>
2022-08-07 02:19:34 -0400

    Better looking ui

--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
 
 export const Balance = () => {
   return (
-    <Paper elevation = {1}>
-    <Typography variant="h5" gutterBottom component="div" align='center'>
-      Current Balance: $3500
-    </Typography>
-    </Paper>
+    <Grid container spacing={2}  justifyContent="center">
+    <Grid item xs={12} md={8}>
+      <Paper elevation = {1}>
+      <Typography variant="h4" gutterBottom component="div" align='center'>
+        Current Balance: $3500
+      </Typography>
+      </Paper>
+    </Grid>
+  </Grid>
+
   )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Typography from '@mui/material/Typography';
 
-
-
 export const Header = () => {
   return (
-  <Typography variant="h3" gutterBottom component="div" align='center'>
+  <Typography variant="h2" gutterBottom component="div" align='center'>
     Expense Tracker App
   </Typography>
 )
